2c023a786950ee855117d66b7be39dc5b19e388a
anil kumar <anilkumarmalluvalasa64@gmail.com>
2026-06-03 22:33:36 +0530

    second commit

--- Web_application/src/Orders.jsx
+++ Web_application/src/Orders.jsx
@@ -4,7 +4,7 @@ import { useSelector, useDispatch } from 'react-redux';
 
 import "./Order.css";
 import { useNavigate } from 'react-router-dom';
-import { addtoCart } from './CartSlice';
+import { addtoCart } from './cartSlice';
 
 function Orders() {
 
